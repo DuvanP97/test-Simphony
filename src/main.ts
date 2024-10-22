dc37a28f12eb46fec1cc85b261a009a092e63b16
Duvan Pedroza <duvan.a.pedroza.n@gmail.com>
2024-10-22 11:42:51 -0500

    add db/swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
