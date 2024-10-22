d7730306d21b48202ea217bb886eee2d0147bbca
Duvan Pedroza <duvan.a.pedroza.n@gmail.com>
2024-10-22 11:42:22 -0500

    create user test

---
+++ src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 201, description: 'User created successfully', type: User })
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Return all users', type: [User] })
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
